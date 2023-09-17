fd4419208da3911cba5f516ba89ad3aea670c03c
DEVANSH VERMA <68294758+devanshverma711@users.noreply.github.com>
2023-09-17 20:00:22 +0530

    First javaScript

---
+++ 41app.js
@@ -0,0 +1,9 @@
+console.log("HEllo World");
+console.log("DEVANSH VERMA");
+let a = 10;
+let b = 5;
+console.log("sum is : " , a+b); 
+let pencil = 10;
+let eraser = 7;
+let output = `The total price is : ${pencil+eraser} Rupees.`;
+console.log(output);
